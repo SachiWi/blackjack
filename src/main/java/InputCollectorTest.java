6534b58cfc3d49c42c2ba4e129753fcb0ed179c2
SachiW <sachinthawijeweera1@gmail.com>
2019-10-22 18:45:09 +1300

    Add files via upload

---
+++ src/main/java/InputCollectorTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputCollectorTest {
+
+    @Test
+    public void collectAction_withPlay_shouldReturnPLAYAction() throws ActionNotSupportedException {
+        InputCollector inputCollector = new InputCollector();
+        Action action = inputCollector.collectAction("play");
+        assertEquals(Action.PLAY, action);
+    }
+
+   
+}
